14be019db28d63a6534dccd043af20fa38b36c53
dev7k <dev7k@protonmail.com>
2019-03-04 13:12:02 +0100

    injected DataContext in the constructor for reading data from sqlite db

--- src/DatingAppApi/Controllers/ValuesController.cs
+++ src/DatingAppApi/Controllers/ValuesController.cs
@@ -2,44 +2,49 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DatingAppApi.Data;
 using Microsoft.AspNetCore.Mvc;
 
-namespace DatingAppApi.Controllers
-{
-    [Route("api/[controller]")]
+namespace DatingAppApi.Controllers {
+    [Route ("api/[controller]")]
     [ApiController]
-    public class ValuesController : ControllerBase
+    public class ValuesController : ControllerBase 
     {
+        private readonly DataContext _context;
+
+        public ValuesController (DataContext context) 
+        {
+            _context = context;
+        }
+
         // GET api/values
         [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
+        public IActionResult GetValues() 
         {
-            return new string[] { "value1", "value2" };
+            var values = _context.Values.ToList();
+
+            return Ok(values);
         }
 
         // GET api/values/5
-        [HttpGet("{id}")]
-        public ActionResult<string> Get(int id)
+        [HttpGet ("{id}")]
+        public IActionResult GetValue (int id) 
         {
-            return "value";
+            var value = _context.Values.FirstOrDefault(x => x.id == id);
+
+            return Ok(value);
         }
 
         // POST api/values
         [HttpPost]
-        public void Post([FromBody] string value)
-        {
-        }
+        public void Post ([FromBody] string value) { }
 
         // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
+        [HttpPut ("{id}")]
+        public void Put (int id, [FromBody] string value) { }
 
         // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
+        [HttpDelete ("{id}")]
+        public void Delete (int id) { }
     }
 }

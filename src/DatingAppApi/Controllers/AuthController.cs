35f5b4667cfe8e3e7797a9c1660416bb50f83fda
dev7k <dev7k@protonmail.com>
2019-03-05 12:57:03 +0100

    added DTO (Data Transfer Object)

--- src/DatingAppApi/Controllers/AuthController.cs
+++ src/DatingAppApi/Controllers/AuthController.cs
@@ -1,5 +1,6 @@
 using System.Threading.Tasks;
 using DatingAppApi.Data;
+using DatingAppApi.Dtos;
 using DatingAppApi.Models;
 using Microsoft.AspNetCore.Mvc;
 
@@ -18,20 +19,20 @@ namespace DatingAppApi.Controllers
 
 
         [HttpPost("register")]
-        public async Task<IActionResult> Register(string username, string password)
+        public async Task<IActionResult> Register(UserForRegisterDto userForRegisterDto)
         {
             // validate request
 
-            username = username.ToLower();
+            userForRegisterDto.Username = userForRegisterDto.Username.ToLower();
 
-            if(await _authRepository.UserExists(username))
+            if (await _authRepository.UserExists(userForRegisterDto.Username))
             {
                 return BadRequest("Username already exists");
             }
 
-            var userToCreate = new User {Username = username};
+            var userToCreate = new User {Username = userForRegisterDto.Username};
 
-            var createdUser = await _authRepository.Register(userToCreate, password);
+            var createdUser = await _authRepository.Register(userToCreate, userForRegisterDto.Password);
 
             return StatusCode(201);
         }

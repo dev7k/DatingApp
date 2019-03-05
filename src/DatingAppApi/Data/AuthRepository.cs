f042e2b4f3efff44102f24245d140f03b2b913f3
dev7k <dev7k@protonmail.com>
2019-03-05 10:43:15 +0100

    implemented tasks for auth repository

--- src/DatingAppApi/Data/AuthRepository.cs
+++ src/DatingAppApi/Data/AuthRepository.cs
@@ -1,21 +1,51 @@
 using System;
 using System.Threading.Tasks;
 using DatingAppApi.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace DatingAppApi.Data {
     public class AuthRepository : IAuthRepository 
     {
         private readonly DataContext _context;
-        
+
         public AuthRepository (DataContext context) 
         {
             _context = context;
         }
 
 
-        public Task<User> Login (string username, string password) 
+        public async Task<User> Login (string username, string password) 
         {
-            throw new System.NotImplementedException ();
+            var user = await _context.Users.FirstOrDefaultAsync(x => x.Username == username);
+
+            if (user == null)
+            {
+                return null;
+            }
+
+            if (!VerifyPasswordHash(password, user.PasswordHash, user.PasswordSalt))
+            {
+                return null;
+            }
+
+            return user;
+        }
+
+        private bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
+        {
+            using (var hmac = new System.Security.Cryptography.HMACSHA512(passwordSalt))
+            {
+                var computedHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+                for(int i = 0; i < computedHash.Length; i++)
+                {
+                    if (computedHash[i] != passwordHash[i])
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
         }
 
         public async Task<User> Register (User user, string password) 
@@ -41,9 +71,14 @@ namespace DatingAppApi.Data {
             }
         }
 
-        public Task<bool> UserExists (string username) 
+        public async Task<bool> UserExists (string username) 
         {
-            throw new System.NotImplementedException ();
+            if(await _context.Users.AnyAsync(x => x.Username == username))
+            {
+                return true;
+            }
+
+            return false;
         }
     }
 }

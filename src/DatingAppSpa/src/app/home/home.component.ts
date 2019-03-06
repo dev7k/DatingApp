2fa3942430f23b86227f50f37db4de927f219cda
dev7k <dev7k@protonmail.com>
2019-03-06 16:27:27 +0100

    Fixed value

--- src/DatingAppSpa/src/app/home/home.component.ts
+++ src/DatingAppSpa/src/app/home/home.component.ts
@@ -15,7 +15,7 @@ export class HomeComponent implements OnInit {
   }
 
   registerToggle() {
-    this.registerMode = !this.registerMode;
+    this.registerMode = true;
   }
 
   cancelRegisterMode(registerMode: boolean) {
